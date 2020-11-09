77a7dd8a92d55cf0da7692ee5b582c55630a54b8
Guilherme Martin <gsilvamartin@gmail.com>
2020-11-09 20:04:54 -0300

    termino gerar tabela

--- gauss-lu.js
+++ gauss-lu.js
@@ -1,11 +1,34 @@
 function gerarMatriz(numero) {
+  var table = "";
   var matrizFinal = [];
 
   for (var i = 0; i <= numero + 1; i++) {
     matrizFinal.push(new Array(numero + 1));
   }
 
-  var table;
+  for (var i = 0; i < matrizFinal.length; i++) {
+    if (i === 0) {
+      table += "<table style='width:100%' class='table'><tr>";
 
-  $('#gauss-table').append();
+      for (var j = 0; j < matrizFinal.length; j++) {
+        if (j === matrizFinal.length - 1) {
+          table += "<td>b<td></tr>";
+        } else {
+          table += "<td>x" + j + "</td>";
+        }
+      }
+    }
+
+    table += "<tr>";
+
+    for (var j = 0; j < matrizFinal.length; j++) {
+        table += "<td><input class='input-number form-control'></td>";
+    }
+
+    table += "</tr>";
+  }
+
+  table += "</table>";
+
+  $("#gauss-table").append(table);
 }
