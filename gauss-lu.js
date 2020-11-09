36aa7a0183a8d3c4e1828a4e818ce4d16fcafff6
Guilherme Martin <gsilvamartin@gmail.com>
2020-11-09 19:39:09 -0300

    inicio generate table

---
+++ gauss-lu.js
@@ -0,0 +1,11 @@
+function gerarMatriz(numero) {
+  var matrizFinal = [];
+
+  for (var i = 0; i <= numero + 1; i++) {
+    matrizFinal.push(new Array(numero + 1));
+  }
+
+  var table;
+
+  $('#gauss-table').append();
+}
