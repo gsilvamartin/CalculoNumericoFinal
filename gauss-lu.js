07a8b24e48a4141c1e295d72a5ac7ca5c7d86c86
Guilherme Martin <gsilvamartin@gmail.com>
2020-11-09 21:07:47 -0300

    atualização gauss

--- gauss-lu.js
+++ gauss-lu.js
@@ -1,16 +1,22 @@
-function gerarMatriz(numero) {
+function gerarMatriz() {
   var table = "";
   var matrizFinal = [];
+  var numero = parseInt($("#gauss-dimension").val());
+
+  if (numero == 0 || numero == null || numero == undefined) {
+    alert("Digite um valor maior que zero.");
+    return;
+  }
 
   for (var i = 0; i <= numero + 1; i++) {
     matrizFinal.push(new Array(numero + 1));
   }
 
-  for (var i = 0; i < matrizFinal.length; i++) {
-    if (i === 0) {
-      table += "<table style='width:100%' class='table'><tr>";
+  for (var i = 1; i < matrizFinal.length - 1; i++) {
+    if (i === 1) {
+      table += "<table id='gauss' style='width:100%' class='table'><tr>";
 
-      for (var j = 0; j < matrizFinal.length; j++) {
+      for (var j = 1; j < matrizFinal.length; j++) {
         if (j === matrizFinal.length - 1) {
           table += "<td>b<td></tr>";
         } else {
@@ -21,14 +27,35 @@ function gerarMatriz(numero) {
 
     table += "<tr>";
 
-    for (var j = 0; j < matrizFinal.length; j++) {
-        table += "<td><input class='input-number form-control'></td>";
+    for (var j = 1; j < matrizFinal.length; j++) {
+      table += "<td><input class='input-number form-control'></td>";
     }
 
     table += "</tr>";
   }
 
-  table += "</table>";
+  table +=
+    "</table><br><button class='btn btn-primary' onclick='calculaGauss()'>Calcular</button>";
 
+  $("#gauss-table").html("");
   $("#gauss-table").append(table);
+}
+
+function calculaGauss() {
+  var finalArray = [];
+  var table = document.getElementById("gauss").querySelectorAll("#gauss tr");
+
+  table.forEach((element) => {
+    line = [];
+    inputs = element.querySelectorAll("input");
+    inputs.forEach((element) => {
+      line.push(element.value);
+    });
+
+    finalArray.push(line);
+  });
+
+  finalArray.shift();
+
+  console.log(finalArray);
 }
